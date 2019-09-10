f05fdd9cee59bbc0853906316e6d7d6bc0c20952
Nishakm1234 <nishakm1234@gmail.com>
2019-09-10 17:24:04 +0530

    my life first git commit

---
+++ f1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+float f=0.1f;
+if(f==0.1f)
+printf("True\n");
+else
+printf("False\n");
+}
---
+++ f2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int x;
+printf("Enter a no\n");
+scanf("%d",&x);
+if(x%2==1||x%2==-1)
+printf("Odd\n");
+else
+printf("Even\n");
+}
